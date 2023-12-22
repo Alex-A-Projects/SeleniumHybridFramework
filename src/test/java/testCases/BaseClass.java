6f3ce419405f9b3dc774aa3e7a7bd72b7dae4fc9
Larin2023 <alexander.v.kolesnyk@gmail.com>
2023-12-22 11:39:38 -0500

    12/22/2023 From work 11:30

--- src/test/java/testCases/BaseClass.java
+++ src/test/java/testCases/BaseClass.java
@@ -42,8 +42,8 @@ public class BaseClass {
         PropertyConfigurator.configure("Log4j.properties");
 
         ChromeOptions opt=new ChromeOptions();
-//        opt.addExtensions(new File("./Extension/1Block.crx"));    // work
-        opt.addExtensions(new File("/Users/oleksii_kolesnik/IdeaProjects/SeleniumHybridFramework/Extention/1Block.crx"));   // home
+        opt.addExtensions(new File("./Extension/1Block.crx"));    // work
+//        opt.addExtensions(new File("/Users/oleksii_kolesnik/IdeaProjects/SeleniumHybridFramework/Extention/1Block.crx"));   // home
         if(br.equals("chrome"))
         {
             System.setProperty("webdriver.chrome.driver",readconfig.getChromePath());
